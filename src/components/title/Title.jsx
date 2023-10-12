9337fed99dfb191cc208113266fd7813e19ed57e
choi11223344 <choi890220@gmail.com>
2023-10-12 10:04:34 +0900

    title

---
+++ src/components/title/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = (props) => {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.desc}</p>
+    </div>
+  );
+};
+export default Title;
